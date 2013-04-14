80db0a5ef1ccf4a35ab9d6b7d68c9cdec516c832
Daniel T. Fortney <dfortney@pod2-1.cs.purdue.edu>
2013-04-14 19:16:27 -0400

    Added main page

--- src/com/example/bit_puzzler/MainActivity.java
+++ src/com/example/bit_puzzler/MainActivity.java
@@ -21,9 +21,9 @@ public class MainActivity extends Activity {
     }
     public void sendMessage(View view){
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
+    	//EditText editText = (EditText) findViewById(R.id.edit_message);
+    	//String message = editText.getText().toString();
+    	//intent.putExtra(EXTRA_MESSAGE, message);
     	startActivity(intent);
     }
 }
