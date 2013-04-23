dd301155e6f2c67511f20bcdbaad9f6c889a0a60
Chris Wendt <chrismwendt@gmail.com>
2013-04-22 21:41:48 -0400

    Disabled add button to avoid multiple submissions, increased font size on puzzle activity

--- src/com/example/bit_puzzler/AddScoreActivity.java
+++ src/com/example/bit_puzzler/AddScoreActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +19,8 @@ public class AddScoreActivity extends Activity {
 	protected Button addButton;
 	protected Button cancelButton;
 	protected EditText player;
-	protected Context context= this;
+	protected Context context = this;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,23 +28,34 @@ public class AddScoreActivity extends Activity {
 		Intent intent = getIntent();
 		score = intent.getIntExtra("HSScore", 1);
 		level = intent.getIntExtra("HSLevel", 1);
-		
-		TextView title = (TextView)findViewById(R.id.TextView01);
-		title.setText("Submit Score Level "+level);
-		TextView tv = (TextView)findViewById(R.id.ScoreText);
-		tv.setText("Level: "+level+"    Score: " + score);
+
+		TextView title = (TextView) findViewById(R.id.TextView01);
+		title.setText("Submit Score Level " + level);
+		TextView tv = (TextView) findViewById(R.id.ScoreText);
+		tv.setText("Level: " + level + "    Score: " + score);
 		player = (EditText) findViewById(R.id.player);
 		addButton = (Button) findViewById(R.id.addButton);
 		addButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-			String player = AddScoreActivity.this.player.getText()
-					.toString();
-			HighScore newScore = new HighScore(player, score);
-			new AddHighScoreTask().execute(newScore);
-		}
-	});
+				((Button) findViewById(R.id.addButton)).setEnabled(false);
+				String player = AddScoreActivity.this.player.getText()
+						.toString();
+				HighScore newScore = new HighScore(player, score);
+				new AddHighScoreTask().execute(newScore);
+			}
+		});
+		cancelButton = (Button)findViewById(R.id.cancelButton);
+		cancelButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View arg0) {
+				finish();
+			}
+		});
+
+		EditText e1 = (EditText)findViewById(R.id.player);
+		e1.setTextColor(Color.WHITE);
 	}
+
 	private class AddHighScoreTask extends AsyncTask<HighScore, Void, Void> {
 
 		protected Void doInBackground(HighScore... highScores) {
@@ -52,12 +66,14 @@ public class AddScoreActivity extends Activity {
 		}
 
 		protected void onPostExecute(Void result) {
+			((Button) findViewById(R.id.addButton)).setEnabled(true);
 			AddScoreActivity.this.finish();
 			Intent intent = new Intent(context, HighScoresActivity.class);
 			intent.putExtra(MainActivity.EXTRA_MESSAGE, level);
 			startActivity(intent);
 		}
 	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
